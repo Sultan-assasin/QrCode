c6ac15fa3e04b8a8fbba993e692e989eae07781a
Sultan-assasin <sulltanadai@gmail.com>
2022-12-22 00:02:50 +0600

    1 commit

---
+++ app/src/main/java/com/ElizaVeta/qrcode/QrViewModel.kt
@@ -0,0 +1,7 @@
+package com.ElizaVeta.qrcode
+
+import androidx.lifecycle.ViewModel
+
+class QrViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/ElizaVeta/qrcode/SignIn.kt
@@ -0,0 +1,50 @@
+package com.ElizaVeta.qrcode
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.ElizaVeta.qrcode.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignIn : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.textView.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, SignUpFragment.newInstance())
+                .commit()
+        }
+
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                       // val intent = Intent(binding.root, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+
+}
