0410b13c0a9100c4fb6fb48e7457d6ce823a8cef
Sultan-assasin <sulltanadai@gmail.com>
2023-01-06 20:56:55 +0600

    qr code generator

--- app/src/main/java/com/ElizaVeta/qrcode/MainActivity.kt
+++ app/src/main/java/com/ElizaVeta/qrcode/MainActivity.kt
@@ -1,14 +1,62 @@
 package com.ElizaVeta.qrcode
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageQRCode: ImageView
+    private lateinit var editText: EditText
+    private lateinit var buttonGenerateQr: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.item_container, SignUpFragment.newInstance())
-            .commit()
+
+        init()
+        clickListener()
+
+    }
+
+    private fun init() {
+        imageQRCode = findViewById(R.id.imageQrCode)
+        editText = findViewById(R.id.etData)
+        buttonGenerateQr = findViewById(R.id.buttonGenerateQr)
+    }
+
+    private fun clickListener() {
+        buttonGenerateQr.setOnClickListener {
+            val data = editText.text.toString().trim()
+            if (data.isEmpty()) {
+                Toast.makeText(this, "Enter some data", Toast.LENGTH_SHORT).show()
+            } else {
+                val writer = QRCodeWriter()
+                try {
+                    val bitMatrix = writer.encode(data, BarcodeFormat.QR_CODE, 512, 512)
+                    val width = bitMatrix.width
+                    val height = bitMatrix.height
+                    val bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+                    for (x in 0 until height) {
+                        for (y in 0 until height) {
+                            bmp.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+                        }
+                    }
+                    imageQRCode.setImageBitmap(bmp)
+                } catch (e: WriterException) {
+                    Log.d("error", e.message.toString())
+                }
+            }
+
+
+        }
     }
 }
--- app/src/main/java/com/ElizaVeta/qrcode/QrViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.ElizaVeta.qrcode
-
-import androidx.lifecycle.ViewModel
-
-class QrViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
